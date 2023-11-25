39b9fadf8c639f994d27b59dd59ca642dfd3d9ed
begona.echavarren@gmail.com <begona.echavarren@gmail.com>
2023-11-25 14:46:40 +0100

    🐛 Fix backend CORS origins

--- backend/app/app/core/config.py
+++ backend/app/app/core/config.py
@@ -1,16 +1,11 @@
 from typing import List
 
-from pydantic import AnyHttpUrl
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    # TODO: Define origins in config
-    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = [
+    BACKEND_CORS_ORIGINS: List[str] = [
         "http://localhost",
-        "http://localhost:4200",
-        "http://localhost:3000",
-        "http://localhost:8080",
     ]
 
 
