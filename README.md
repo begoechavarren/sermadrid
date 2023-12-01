76ee5f5b9f308d7209fb0d4fb0c7fce3f48799ae
begona.echavarren@gmail.com <begona.echavarren@gmail.com>
2023-12-01 18:26:53 +0100

    🚚 Rename project

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# HelloWorld
+# SERMadrid
+```
+$ docker-compose build --no-cache
+$ docker-compose up -d
+```
+
+http://localhost:80
+
+- datetime: `2024-01-01T12:30:00`
+- latitude: `40.416775`
+- longitude: `-3.703790`
--- backend/app/app/core/config.py
+++ backend/app/app/core/config.py
@@ -4,7 +4,12 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    BACKEND_CORS_ORIGINS: List[str] = ["http://localhost", "http://localhost:8080"]
+    BACKEND_CORS_ORIGINS: List[str] = [
+        "http://localhost",
+        "http://localhost:8080",
+        "http://0.0.0.0",
+        "http://0.0.0.0:80",
+    ]
 
 
 settings = Settings()
