f9f81112fc65d4ad7ac749fff3fee39dbaeb5822
Begoña Echavarren Sanchez <begona.echavarren@gmail.com>
2023-12-06 19:24:20 +0100

    🐛 Fix get Github action variables

--- backend/app/app/core/config.py
+++ backend/app/app/core/config.py
@@ -1,7 +1,11 @@
+# TODO: remove
+import os
 from typing import List
 
 from pydantic import Field
 from pydantic_settings import BaseSettings
+
+print("BACKEND_CORS_ORIGINS from env:", os.getenv("BACKEND_CORS_ORIGINS"))
 
 
 class Settings(BaseSettings):
