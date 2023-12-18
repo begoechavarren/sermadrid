050b77f5cd8cae1ac6fac115c3ef7050cc32d63d
Begoña Echavarren Sanchez <begona.echavarren@gmail.com>
2023-12-18 11:06:04 +0100

    ✨ Add generate terraform backend deployment step

--- infrastructure/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket     = var.aws_s3_bucket_name
-    key        = "terraform.tfstate"
-    region     = var.aws_region
-    access_key = var.aws_access_key
-    secret_key = var.aws_secret_key
-  }
-}
--- infrastructure/digital_ocean.tf
+++ infrastructure/digital_ocean.tf
@@ -2,11 +2,6 @@ provider "digitalocean" {
   token = var.do_token
 }
 
-resource "digitalocean_spaces_bucket" "bucket" {
-  name   = "sermadrid"
-  region = local.do_region
-}
-
 resource "digitalocean_droplet" "droplet" {
   image    = "ubuntu-23-10-x64"
   name     = "ubuntu-s-1vcpu-512mb-10gb-ams3-01"
