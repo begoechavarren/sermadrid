04ff6b5cf5218aa17bd4fd44ca52f185e697ad17
Begoña Echavarren Sanchez <begona.echavarren@gmail.com>
2024-01-12 21:43:19 +0100

    🐳 Add SSL certificate to frontend volume

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -6,8 +6,13 @@ nginx &
 # Wait for Nginx to start
 sleep 5
 
-# Obtain the SSL certificate
-certbot certonly --webroot -w /usr/share/nginx/html -d sermadrid.org -d www.sermadrid.org --email begona.echavarren@gmail.com --agree-tos --non-interactive --no-eff-email
+# Check if a valid SSL certificate already exists and if not, obtain a new one
+if [ ! -d "/etc/letsencrypt/live/sermadrid.org" ] || ! certbot certificates | grep -q "sermadrid.org"; then
+    echo "Obtaining new SSL certificate for sermadrid.org"
+    certbot certonly --webroot -w /usr/share/nginx/html -d sermadrid.org -d www.sermadrid.org --email begona.echavarren@gmail.com --agree-tos --non-interactive --no-eff-email
+else
+    echo "Existing SSL certificate found for sermadrid.org"
+fi
 
 # Add Certbot renew to crontab
 echo "0 12 * * * certbot renew --webroot -w /usr/share/nginx/html" >> /etc/crontabs/root
