53dc72ff30a8f5a2d33bdc61bdac912b5661f1ff
Begoña Echavarren Sanchez <begona.echavarren@gmail.com>
2024-01-14 23:34:25 +0100

    ✨ Add map SER zone mask

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,85 +1,211 @@
 <template>
   <div class="home">
     <h1>sermadrid</h1>
-    <p>Select the location and time where you would like to park in the Madrid SER zone</p>
+    <p>
+      Select the location and time where you would like to park in the Madrid
+      SER zone
+    </p>
     <div id="map" class="map-container"></div>
+    <div
+      v-show="showOutsideMessage"
+      class="outside-message"
+      :style="messageStyle"
+    >
+      Out of the SER zone
+    </div>
     <form @submit.prevent="getItem" class="availability-form">
       <div class="form-group">
-        <flat-pickr v-model="datetime" :config="config" class="form-control" placeholder="Select date and time"></flat-pickr>
+        <flat-pickr
+          v-model="datetime"
+          :config="config"
+          class="form-control"
+          placeholder="Select date and time"
+        ></flat-pickr>
       </div>
-      <button type="submit" class="availability-button">Get Availability</button>
+      <button type="submit" class="availability-button">
+        Get Availability
+      </button>
     </form>
-      <div v-if="itemResult" class="result-output">
-        {{ itemResult }}
-      </div>
+    <div v-if="itemResult" class="result-output">
+      {{ itemResult }}
+    </div>
   </div>
 </template>
 
 <script>
-import mapboxgl from 'mapbox-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
-import FlatPickr from 'vue-flatpickr-component';
-import 'flatpickr/dist/flatpickr.css';
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import FlatPickr from "vue-flatpickr-component";
+import "flatpickr/dist/flatpickr.css";
+import * as turf from "@turf/turf";
+import limiteZonaSer from "../assets/ser_zone_limit.geojson"; // Replace with your actual path
 
 export default {
-  name: 'HomeComponent',
+  name: "HomeComponent",
   components: {
-    FlatPickr
+    FlatPickr,
   },
   data() {
     return {
-      datetime: '',
+      datetime: "",
       itemResult: null,
       map: null,
       marker: null,
+      latitude: null,
+      longitude: null,
+      showOutsideMessage: false,
+      messagePosition: { x: 0, y: 0 },
       config: {
         enableTime: true,
         dateFormat: "Y-m-d H:i",
         defaultDate: new Date(),
         minDate: "today",
-        time_24hr: true
-      }
+        time_24hr: true,
+      },
     };
   },
   mounted() {
     mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_TOKEN;
     this.map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-3.7037902, 40.4167754],
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [-3.694241, 40.4167754],
       zoom: 12
     });
 
-    this.map.addControl(new mapboxgl.NavigationControl());
+    const updateMask = () => {
+      const bounds = this.map.getBounds().toArray().flat();
+      const expandedBounds = [
+        bounds[0] - 1, // west (left) bound - expand by 0.1 degrees
+        bounds[1] - 1, // south (bottom) bound - expand by 0.1 degrees
+        bounds[2] + 1, // east (right) bound - expand by 0.1 degrees
+        bounds[3] + 1, // north (top) bound - expand by 0.1 degrees
+      ];
+      const bboxPoly = turf.bboxPolygon(expandedBounds);
+      const maskGeometry = limiteZonaSer.features[0].geometry;
+      let firstPolygonGeometry;
 
-    this.map.on('load', () => {
-      this.marker = new mapboxgl.Marker();
-      this.map.on('click', (e) => {
+      if (maskGeometry.type === "MultiPolygon") {
+        firstPolygonGeometry = {
+          type: "Polygon",
+          coordinates: maskGeometry.coordinates[0],
+        };
+      } else {
+        firstPolygonGeometry = maskGeometry;
+      }
+
+      const mask = turf.difference(bboxPoly, firstPolygonGeometry);
+
+      if (!mask) {
+        console.error("Unable to create mask from the given MultiPolygon.");
+        return;
+      }
+
+      if (this.map.getSource("mask")) {
+        this.map.getSource("mask").setData(mask);
+      } else {
+        this.map.addSource("mask", {
+          type: "geojson",
+          data: mask,
+        });
+
+        this.map.addLayer({
+          id: "mask",
+          source: "mask",
+          type: "fill",
+          paint: {
+            "fill-color": "#000",
+            "fill-opacity": 0.5,
+          },
+        });
+      }
+    };
+
+    this.map.on("load", updateMask);
+    this.map.on("moveend", updateMask);
+
+    this.map.on("click", this.mapClickHandler);
+  },
+  methods: {
+    mapClickHandler(e) {
+      const point = turf.point([e.lngLat.lng, e.lngLat.lat]);
+      const within = turf.booleanPointInPolygon(
+        point,
+        limiteZonaSer.features[0].geometry
+      );
+      if (within) {
         this.latitude = e.lngLat.lat;
         this.longitude = e.lngLat.lng;
         if (this.marker) {
-          this.marker.remove(); // Remove the previous marker if there is one
+          this.marker.remove();
         }
         this.marker = new mapboxgl.Marker()
           .setLngLat([this.longitude, this.latitude])
           .addTo(this.map);
-      });
-    });
-  },
-  methods: {
+      } else {
+        // Show the message and update the position
+        this.messagePosition = {
+          x: e.originalEvent.clientX,
+          y: e.originalEvent.clientY,
+        };
+        this.showOutsideMessage = true;
+        // Use a timeout to hide the message after 2 seconds
+        clearTimeout(this.hideMessageTimeout);
+        this.hideMessageTimeout = setTimeout(() => {
+          this.showOutsideMessage = false;
+        }, 2000);
+      }
+    },
+    createMask(maskFeatureCollection, bounds) {
+      var bboxPoly = turf.bboxPolygon(bounds);
+      var maskGeometry = maskFeatureCollection.features[0].geometry;
+
+      // Check if the geometry is a MultiPolygon and use the first Polygon
+      if (maskGeometry.type === "MultiPolygon") {
+        // Extract the first polygon's coordinates from the MultiPolygon
+        var firstPolygonCoordinates = maskGeometry.coordinates[0];
+        // Create a Polygon geometry from the first set of coordinates
+        maskGeometry = {
+          type: "Polygon",
+          coordinates: firstPolygonCoordinates,
+        };
+      }
+
+      // Perform the difference operation
+      var masked = turf.difference(bboxPoly, maskGeometry);
+
+      // Check if the operation was successful
+      if (!masked) {
+        throw new Error(
+          "Difference operation failed. The mask might not be subtracting correctly."
+        );
+      }
+
+      return masked;
+    },
     async getItem() {
       try {
-        const response = await fetch(`/api/v1/items/datetime/${this.datetime}/latitude/${this.latitude}/longitude/${this.longitude}`);
+        const response = await fetch(
+          `/api/v1/items/datetime/${this.datetime}/latitude/${this.latitude}/longitude/${this.longitude}`
+        );
         if (!response.ok) {
-          throw new Error('Network response was not ok.');
+          throw new Error("Network response was not ok.");
         }
         const data = await response.json();
         this.itemResult = `For the datetime ${this.datetime} and location lat: ${this.latitude}, long: ${this.longitude}, the parking availability is: ${data.result}`;
       } catch (error) {
-        this.itemResult = 'Error fetching availability: ' + error.message;
+        this.itemResult = "Error fetching availability: " + error.message;
       }
-    }
-  }
+    },
+  },
+  computed: {
+    messageStyle() {
+      return {
+        top: this.messagePosition.y + "px",
+        left: this.messagePosition.x + "px",
+      };
+    },
+  },
 };
 </script>
 
@@ -96,7 +222,7 @@ export default {
 
 .availability-form .form-group {
   display: flex;
-  flex-direction: column
+  flex-direction: column;
 }
 
 .availability-form .form-group {
@@ -129,11 +255,20 @@ export default {
 }
 
 .result-output {
-    font-weight: bold;
-    text-align: center;
-    margin-top: 20px;
-    font-size: 1em;
-  }
+  font-weight: bold;
+  text-align: center;
+  margin-top: 20px;
+  font-size: 1em;
+}
 
+.outside-message {
+  position: fixed;
+  background: white;
+  padding: 5px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  pointer-events: none;
+  transform: translate(-50%, -50%);
+  z-index: 1000; /* Ensure it's above map elements */
+}
 </style>
-
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+
+  chainWebpack: config => {
+    // Add a new rule for .geojson files
+    config.module
+      .rule('geojson')
+      .test(/\.geojson$/)
+      .use('json-loader')
+        .loader('json-loader')
+        .end();
+  }
+});
