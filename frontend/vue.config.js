f78e18a64052d519bc3a8702a0b39108566f1f6b
Begoña Echavarren Sanchez <begona.echavarren@gmail.com>
2024-01-18 20:37:13 +0100

    💄 Update favicon and application title

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,6 +3,15 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
 
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'sermadrid',
+    },
+  },
+
   chainWebpack: config => {
     // Add a new rule for .geojson files
     config.module
