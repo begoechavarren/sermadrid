bd269fe1f68353d6445890973424d380c1790a77
Begoña Echavarren Sanchez <begona.echavarren@gmail.com>
2024-05-16 22:08:02 +0200

    ✨ Add Flower

--- backend/app/app/celery_app.py
+++ backend/app/app/celery_app.py
@@ -1,10 +1,18 @@
 from celery import Celery
 
-app = Celery(
-    "worker",
-    broker="redis://redis:6379/0",
-    include=["app.app.tasks"],
+app = Celery("app", broker="redis://redis:6379/0")
+
+app.conf.update(
+    result_backend="redis://redis:6379/0",
+    task_serializer="json",
+    accept_content=["json"],
+    result_serializer="json",
+    timezone="UTC",
+    enable_utc=True,
 )
+
+app.autodiscover_tasks(["app.app.tasks"])
+
 
 if __name__ == "__main__":
     app.start()
